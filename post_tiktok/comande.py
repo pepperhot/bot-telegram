7548ea469278e032909abbe00f95898ca7d0303d
pepperhot <lucas.gemo77@gmail.com>
2025-10-06 21:57:05 +0200

    arborecence

---
+++ post_tiktok/comande.py
@@ -0,0 +1,9 @@
+async def start(update, _):
+    if update.message:
+        user = update.message.from_user
+    elif update.effective_user:
+        user = update.effective_user
+    else:
+        return
+    await update.message.reply_text(f"Salut {user.first_name}, voici mon bot telegram! ")
+    await update.message.reply_text("Commandes:\n    - /start : affiche ce message \n    -/lyrics : format de tiktok karoucelle \n    - /pallette : affiche les couleurs disponibles \n    -/karaoke : format de tiktok karaoke avec videos")
